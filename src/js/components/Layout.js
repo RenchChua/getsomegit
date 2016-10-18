7e1570b62dc6b695fd1cbac5364881946b98a075
Rencheng Chua <rench00@gmail.com>
2016-10-18 12:27:59 +0800

    do functions to show the details of each git repo. Left with showing the languages

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -9,28 +9,104 @@ export default class Layout extends React.Component {
     this.state = {
       title: "Welcome",
       searchResults: [],
+      languages: [],
+      description:[],
+      followers:[]
     };
+    this.showDetails=this.showDetails.bind(this);
+    this.searchKeywords=this.searchKeywords.bind(this);
   }
 
   searchKeywords(e){
     e.preventDefault()
     var searchKeywords = this.refs.searchTerms.value
-    axios.get('https://api.github.com/search/repositories?q=' + searchKeywords)
-      .then(function(response){
-        this.setState({searchResults:response.data.items})
-        console.log(this.state.searchResults);
+    if (!searchKeywords) {
+      this.setState({searchResults:[]})
+      $(".none-found").hide()
+      $(".no-keywords").show()
+    }else{
+      $(".no-keywords").hide()
+      axios.get('https://api.github.com/search/repositories?q=' + searchKeywords)
+        .then(function(response){
+          this.setState({searchResults:response.data.items})
+          console.log(response.data.items);
+          if (this.state.searchResults.length === 0) {
+            $(".none-found").show()
+          }
 
-      }.bind(this));
+        }.bind(this));
+    }
+  }
+
+  showDetails(e){
+    let eventID = e.target.id
+    let resultID = parseInt(eventID.substring(3))
+    let resultIDtype = typeof resultID
+    for (var i = 0; i < this.state.searchResults.length; i++) {
+      let elementID = "#re-" + this.state.searchResults[i].id
+      if (this.state.searchResults[i].id === resultID) {
+        let userName = this.state.searchResults[i].owner.login
+        console.log("the user selected is ", userName);
+        let resultDescription = this.state.searchResults[i].description
+        $(elementID).show()
+
+        // display description with some trimming to handle very long description
+        if (!resultDescription) {
+          this.setState({description:"No description"})
+        } else{
+          if (resultDescription.length > 100) {
+            let trimmedDescription = resultDescription.substring(0,100) + "..."
+            this.setState({description:trimmedDescription})
+          }else{
+            this.setState({description:resultDescription})
+          }
+
+          // display number of followers of user (Note: GitHub doesn't let people follow specific repos)
+
+          axios.get('https://api.github.com/users/' + userName + '/followers' )
+            .then(function(response){
+              let numOfFollowers = response.data.length
+              if (!numOfFollowers) {
+                this.setState({followers:"This user has no followers"})
+              } else{
+                this.setState({followers:numOfFollowers})
+              }
+
+            }.bind(this));
+
+
+
+
+        }
+      }else{
+        $(elementID).hide()
+      }
+    }
   }
 
   render() {
-    const resultList = this.state.searchResults.map( result =>
-      <div key={result.id} id={result.id}>
-        <div className="result-item-container col-lg-12 col-xs-12">
-          <img className="avatar_img" src={result.owner.avatar_url} alt="avatar_url" />
-          <h2>{result.full_name}</h2>
-        </div>
-      </div>)
+    let resultList
+    if (this.state.searchResults.length === 0) {
+      resultList = <div className="none-found"><h3>No repos found. Please key in another search term</h3></div>
+    }else{
+      let resultDetail
+      resultList = this.state.searchResults.map( result =>
+        <div key={result.id} id={result.id}>
+          <div id={"rc-" + result.id} className="result-item-container col-lg-12 col-xs-12" onClick={this.showDetails}>
+            <div className="result-basic">
+              <img id={"ri-" + result.id} className="avatar_img" src={result.owner.avatar_url} alt="avatar_url" />
+              <h2 id={"rn-" + result.id} >{result.full_name}</h2>
+            </div>
+            <div id={"re-" + result.id} className="result-expanded">
+              <a id={"ra-" + result.id} href={result.html_url} target="_blank"> {result.html_url}</a>
+              <h3 id={"rd-" + result.id}> Description: {this.state.description}</h3>
+              <h3 id={"rw-" + result.id} className="watchers-count"><i class="fa fa-eye" aria-hidden="true"></i> {result.watchers_count}</h3>
+              <h3 id={"rf-" + result.id} className="followers-count">{"Number of followers: " + this.state.followers}</h3>
+            </div>
+          </div>
+        </div>)
+    }
+
 
     return (
       <div>
@@ -39,6 +115,9 @@ export default class Layout extends React.Component {
           <button className="btn keywords-btn" onClick={this.searchKeywords.bind(this)}>
             <i class="fa fa-search" aria-hidden="true"></i>
           </button>
+        </div>
+        <div className="no-keywords">
+          <h3>Please enter some search terms</h3>
         </div>
         <div className="results-container col-lg-12">
           <div className="show-results col-lg-9">
